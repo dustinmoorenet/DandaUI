00f9cbe5d4907be574a80fa8bf61d7244a0a0ca5
Dustin Moore <dustin@dandamoore.com>
2013-08-29 22:06:52 -0500

    Added grunt system

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    js_src: ['g/**/*.js', 'u/**/*.js'],
+    jshint: {
+      dist: {
+        src: '<%= js_src %>',
+        options: {
+          curly: true,
+          eqeqeq: true,
+          immed: true,
+          latedef: true,
+          newcap: true,
+          noarg: true,
+          sub: true,
+          undef: true,
+          boss: true,
+          eqnull: true
+        }
+      }
+    },
+    concat: {
+      dist: {
+        src: '<%= js_src %>',
+        dest: 'dist/danda-ui.js'
+      }
+    },
+    uglify: {
+      dist : {
+        src : 'dist/danda-ui.js',
+        dest : 'dist/danda-ui.min.js'
+      }
+    },
+    stylus: {
+      compile: {
+        files: {
+          'dist/danda-ui.css': 'css/**.styl'
+        }
+      }
+    },
+    watch: {
+      files: ['<%= js_src %>', 'css/**.styl'],
+      tasks: ['jshint', 'concat', 'uglify', 'stylus']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch'); 
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'stylus']);
+};
---
+++ dist/danda-ui.js
@@ -0,0 +1,9 @@
+/**
+ * The base graphical (G) user interface object
+ */
+var G = {};
+
+/**
+ * The utility library
+ */
+var U = {};
